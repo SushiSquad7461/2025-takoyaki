80f5d2e8c64d7401f5b2f102a43fc670b108200b
swetha-mandaleeka <swethamandaleeka@icloud.com>
2025-02-08 10:45:02 -0800

    Robot controls with empty commands updated for feedback

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 public class RobotContainer {
+    public RobotContainer(){
+        CommandXboxController driverController = new CommandXboxController(0);
+        CommandXboxController operatorController = new CommandXboxController(1); 
+        operatorController.a().onTrue(Commands.idle()); //Level 1
+        operatorController.x().onTrue(Commands.idle()); //Level 2
+        operatorController.y().onTrue(Commands.idle()); //Level 3
+        operatorController.b().onTrue(Commands.idle()); //Level 4
+
+        driverController.leftBumper().whileTrue(Commands.idle());  // intake wheels rolled in regular direction
+        driverController.leftBumper().onFalse(Commands.idle());    // raise intake
+        driverController.rightBumper().whileTrue(Commands.idle()); // intake wheels rolled in reverse
     
+    }
 }
+
+
