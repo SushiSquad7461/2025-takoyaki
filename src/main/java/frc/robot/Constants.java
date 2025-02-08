0546cb2c1d1e73bae5adb1ad9fc6ab9d51466736
shobsbh <114537963+shobsbh@users.noreply.github.com>
2025-02-08 12:23:52 -0800

    worked on elevator subsystem, imported sushi lib (#1)
    * worked on elevator subsystem, imported sushi lib
    
    * revised the elevator code, updated sushilib
    
    * elevator updates
    
    * updated elevator, w units
    
    * changed packages to be lowercase
    
    * fixed setting encoder limits to use units library
    
    * fixed package declarations
    
    * made lowercase package declarations (oops)
    
    * fixed
    
    * fixed units for curr spike
    
    * fixed units again!!!! nd tunable numbers
    
    * acct fixed
    
    * units....
    
    * used slot0 configs
    
    * copiym
    
    * cleanup imports and remove unused class
    
    ---------
    
    Co-authored-by: shobsbh <1075665@lwsd.org>
    Co-authored-by: Samuel P. Jones <spjones329@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,62 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.derive;
+
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.DistanceUnit;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.PerUnit;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Distance;
+import frc.util.control.PIDConfig;
+import frc.util.motor.MotorConfig;
+
 public class Constants {
+    public static boolean TUNING_MODE;
+
+    public static class CustomUnits {
+        public static final AngleUnit TalonEncoderCounts = derive(Rotations).splitInto(2048).named("Talon Encoder Counts").symbol("TEC").make();
+        public static final PerUnit<DistanceUnit, AngleUnit> MetersPerRotation = Meters.per(Rotations);
+    }
+
+    public static class Elevator {
+        //creating config for software limit switch bec sushi lib doesnt handle
+        //TODO: use sysid
+        public static final double kG = 0;
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
     
+        public static final MotorConfig ELEVATOR_LEFT = new MotorConfig(
+            0,
+            0,
+            true,
+            PIDConfig.getPid(0.0),
+            MotorConfig.Mode.BRAKE);
+
+        public static final MotorConfig ELEVATOR_RIGHT = new MotorConfig(
+            0,
+            0,
+            false,
+            PIDConfig.getPid(0.0, 0.0, 0.0),
+            MotorConfig.Mode.BRAKE);
+
+        // TODO: set all of these values
+        public static final Angle MAX_ERROR = Degrees.of(1.0);
+
+        public static final int LIMIT_SWITCH_PORT = 0; 
+
+        public static final Distance MAX_HEIGHT = Inches.of(45.0); 
+        public static final Measure<? extends PerUnit<DistanceUnit, AngleUnit>> ELEVATOR_EXTENSION_PER_MOTOR_ANGLE = CustomUnits.MetersPerRotation.of(0);
+        public static final Dimensionless GEAR_RATIO = Rotations.of(0).div(Rotations.of(1)); // output over input
+        public static final Angle MOTOR_MAX_HEIGHT = (Angle) MAX_HEIGHT.div(ELEVATOR_EXTENSION_PER_MOTOR_ANGLE).div(GEAR_RATIO);
+    }
 }
---
+++ src/main/java/frc/util/Motor/MotorConfig.java
@@ -0,0 +1,105 @@
+package frc.util.motor;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import frc.util.control.PIDConfig;
+import frc.util.control.SmartDashboard.PIDTuning;
+
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class MotorConfig {
+   public final PIDConfig pid;
+   public final int canId;
+   public final String canBus;
+   public final int currentLimit; // A currentLimit of bellow zero will not be set on the motor
+   public final boolean inversion;
+   public final Mode mode;
+
+   public enum Mode {
+        COAST(true),
+        BRAKE(false);
+
+        private boolean mode;
+
+        private Mode(boolean mode) {
+            this.mode = mode;
+        }
+
+        public NeutralModeValue getTalonMode() {
+            return mode ? NeutralModeValue.Coast : NeutralModeValue.Brake;
+        }
+
+        public IdleMode getSparkMaxMode() {
+            return mode ? IdleMode.kCoast : IdleMode.kBrake;
+        }
+   };
+
+   public MotorConfig(int canId, String canBus, int currentLimit, Boolean inversion, PIDConfig pid, Mode mode) {
+        this.canId = canId;
+        this.canBus = canBus;
+        this.currentLimit = currentLimit;
+        this.inversion = inversion;
+        this.pid = pid;
+        this.mode = mode;
+   }
+
+   public MotorConfig(int canId, int currentLimit, Boolean inversion, PIDConfig pid, Mode mode) { this(canId, "rio", currentLimit, inversion, pid, mode); }
+
+public PIDTuning genPIDTuning(String motorName, boolean tuningMode) {
+        return new PIDTuning(motorName, pid, tuningMode);
+   }
+
+   public TalonFXConfiguration getTalonConfig() {
+        TalonFXConfiguration talonConfig = new TalonFXConfiguration();
+
+        MotorHelper.updateSupplyCurrentLimit(currentLimit, talonConfig);
+ 
+        talonConfig.MotorOutput.NeutralMode = mode.getTalonMode();
+        talonConfig.MotorOutput.Inverted = inversion ? InvertedValue.CounterClockwise_Positive : InvertedValue.Clockwise_Positive;
+
+        pid.updatePidConfig(talonConfig);
+
+        return talonConfig;
+   }
+
+   public TalonFX createTalon() {
+        TalonFX motor = new TalonFX(canId, canBus);
+        motor.getConfigurator().apply(getTalonConfig());
+        return motor;
+   }
+
+   public void setCanSparkMaxConfig(SparkMax motor, MotorType type) {
+     SparkMaxConfig config = new SparkMaxConfig();
+     config.inverted(inversion);
+     config.smartCurrentLimit(currentLimit);
+     config.idleMode(mode.getSparkMaxMode());
+     config.closedLoop.apply(pid.createSparkMaxConfig());
+
+     motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+     if (type == MotorType.kBrushless) {
+          motor.getEncoder().setPosition(0);
+     }
+
+   }
+
+   public SparkMax createSparkMax() { return createSparkMax(MotorType.kBrushless); }
+
+   public SparkMax createSparkMax(MotorType type) {
+        SparkMax motor = new SparkMax(canId, type);
+        setCanSparkMaxConfig(motor, type);
+        return motor;
+   }
+
+   public MotorConfig withCanId(int canId) {
+     return new MotorConfig(canId, this.canBus, this.currentLimit, this.inversion, this.pid, this.mode);
+   }
+}
