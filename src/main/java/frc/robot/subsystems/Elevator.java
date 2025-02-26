95c1a3b5dd6eb88c65cd118a7fd1db4d7074ee80
shobsbh <1075665@lwsd.org>
2025-02-25 18:08:37 -0800

    added motion magic

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -10,6 +10,7 @@ import java.util.function.BooleanSupplier;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -33,6 +34,7 @@ public class Elevator extends SubsystemBase {
   private final TalonFX leftMotor;
   private final TalonFX rightMotor;
   private final DigitalInput limitSwitch;
+  private final MotionMagicVoltage motionMagic;
 
   private final NetworkTable elevatorTable;
   private final DoublePublisher positionPub;
@@ -51,6 +53,7 @@ public class Elevator extends SubsystemBase {
     elevatorRightPID = Constants.Elevator.ELEVATOR_LEFT.genPIDTuning("Elevator", Constants.TUNING_MODE);
     leftMotor = Constants.Elevator.ELEVATOR_LEFT.createTalon();
     rightMotor = Constants.Elevator.ELEVATOR_RIGHT.createTalon();
+    motionMagic = new MotionMagicVoltage(0);
 
     // network table variables
     elevatorTable = NetworkTableInstance.getDefault().getTable("Elevator");
@@ -94,7 +97,7 @@ public class Elevator extends SubsystemBase {
   public Command changeState(ElevatorState state) {
     return run(() -> {
       elevatorTable.getDoubleTopic("Setpoint").publish().set(state.getPos().in(Inches));
-      rightMotor.setControl(new PositionDutyCycle(heightToMotor(state.position)));
+      rightMotor.setControl(motionMagic.withPosition(heightToMotor(state.position)));
       if(state != ElevatorState.IDLE) reset = false;
     }).until(elevatorInPosition(state))
         .andThen(state == ElevatorState.IDLE && !reset ? resetElevator() : Commands.none());
@@ -168,8 +171,8 @@ public class Elevator extends SubsystemBase {
   }
 
 
-public Command goDown() {
-  return run(() -> rightMotor.set(-.2)).finallyDo(() -> rightMotor.set(0));
-}
+  public Command goDown() {
+    return run(() -> rightMotor.set(-.2)).finallyDo(() -> rightMotor.set(0));
+  }
 
 }
